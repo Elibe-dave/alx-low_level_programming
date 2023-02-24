b370120beec22a27bef1565b672402c26688539b
Elibe-dave <elibe.dave@gmail.com>
2023-02-23 17:38:25 -0800

    pushiing tasks

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _isupper - this checks for uppercase character
+* @c: Variable dependent text
+* Return: This will always 0
+*/
+int _isupper(int c)
+{
+
+
+ if (c >= 'A' && c <= 'Z')
+ {
+ return (1);
+ }
+ else
+{
+ return (0);
+}
+
+}
