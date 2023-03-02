798c8e43e6e0e3f575b98a8f15e4e73152d72849
Elibe-dave <elibe.dave@gmail.com>
2023-03-02 15:45:43 -0800

    task

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -8,21 +8,16 @@ int main(void)
 
   a[2] = 1024;
   p = &n;
-
   /*
-   * Add the following line of code to assign the value 98 to a[2]
-   * without using the variable a or modifying p.
+   * write your line of code here...
    * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-
-  int diff = p - &n;  // calculate the difference between p and &n
-  int *addr = &a[2] - diff;  // calculate the address of a[2] in the context of p and n
-  *addr = 98;  // assign the value 98 to the memory location pointed to by addr
-
-  (void) printf("%d", *addr);  // print the value 98
-
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
   return (0);
 }
