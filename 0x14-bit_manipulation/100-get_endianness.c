47377e47f8028c07e9e4cf91e043d3b95235acfd
Elibe-dave <elibe.dave@gmail.com>
2023-04-10 20:18:42 -0700

    updated 100-get_endianness.c to comply with betty

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,20 +1,12 @@
 #include "main.h"
+/**
+ * get_endianness - checks a machine is endian size
+ * Return: 0 for big, 1 for little
+ */
 
-/*
- * get_endianness - Checks the endianness of the current system.
- * Description: A function that determines whether the current system is
- * big-endian or little-endian. The function returns 1
- * if the system is little-endian, and 0 if it is big-endian.
- * Return: 1 if the system is little-endian, 0 if it is big-endian.
-*/
 int get_endianness(void)
 {
 unsigned int i = 1;
 char *c = (char *) &i;
-
-/*
- * If the first byte of the integer representation of 1 is 1
- * the system is little-endian. Otherwise, it is big-endian.
-*/
 return (*c);
 }
