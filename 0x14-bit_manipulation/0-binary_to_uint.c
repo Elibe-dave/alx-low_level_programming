2ac850b82ff85d258fd3536a5fa2e8f1b3e25074
Elibe-dave <elibe.dave@gmail.com>
2023-05-05 22:10:30 -0700

    updated 0-binary_to_uint.c to reflect correct prototye usage

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,21 +1,21 @@
 /**
  * binary_to_uint - converts a binary number string to unsigned integer
- * @binary_str: string containing the binary number
+ * @b: string containing the binary number
  * Return: the converted unsigned integer
  **/
-unsigned int binary_to_uint(const char *binary_str)
+unsigned int binary_to_uint(const char *b)
 {
 int i;
 unsigned int decimal_val = 0;
 
-if (!binary_str)
+if (!b)
 return (0);
 
-for (i = 0; binary_str[i]; i++)
+for (i = 0; b[i]; i++)
 {
-if (binary_str[i] < '0' || binary_str[i] > '1')
+if (b[i] < '0' || b[i] > '1')
 return (0);
-decimal_val = 2 * decimal_val + (binary_str[i] - '0');
+decimal_val = 2 * decimal_val + (b[i] - '0');
 }
 
 return (decimal_val);
