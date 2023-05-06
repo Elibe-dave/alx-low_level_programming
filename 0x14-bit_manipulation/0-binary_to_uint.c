c18ba67444137ce74d1eb2976f8c3e08863fb84c
Elibe-dave <elibe.dave@gmail.com>
2023-05-05 22:30:29 -0700

    updated 0-binary_to_uint.c to reflect correct prototye usage

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,13 +11,18 @@ unsigned int decimal_val = 0;
 if (!b)
 return (0);
 
-for (i = 0; b[i]; i++)
+while (*b != '\0')
 {
-if (b[i] < '0' || b[i] > '1')
+if (*b == '0')
+{
+converted = (converted << 1);
+}
+else if (*b == '1')
+converted = (converted << 1) + 1;
+}
+else
+{
 return (0);
-decimal_val = 2 * decimal_val + (b[i] - '0');
-}
-
-return (decimal_val);
-}
+b++;
+return (converted);
 }
